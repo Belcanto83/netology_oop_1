6826e6d291b0be72c170a5af6407418c3981e467
Alex <farego@yandex.ru>
2022-07-17 12:49:13 +0300

    Initial commit. Tasks 1-2

---
+++ main.py
@@ -0,0 +1,51 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+student_1 = Student('Ivan', 'Petrov', 'male')
+lector_1 = Lecturer('Nicolay', 'Lobachevskiy')
+
+student_1.courses_in_progress.append('Python')
+lector_1.courses_attached.append('Python')
+
+student_1.rate_lecturer(lector_1, 'Python', 5)
+student_1.rate_lecturer(lector_1, 'Python', 4)
